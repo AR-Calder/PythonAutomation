52b9ba537458324e482d0b0737d0e58840ee7b85
Andrew Calder <Androbcal@gmail.com>
2018-11-20 16:40:15 +0000

    added notification for output vis

--- wccount.py
+++ wccount.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 import tkinter as tk
+import os
 
 window = tk.Tk();
 window.withdraw()
 
 # Prints number of chracters and words in current selection
-print("characters: %s" % len(window.selection_get()))
-print("words: %s" % len(window.selection_get().split()))
+# Only tested in gnome3 environments
+
+characters = len(window.selection_get())
+words = len(window.selection_get().split())
+
+print("characters: %s" % characters)
+print("words: %s" % words)
+os.system("notify-send 'wccount' 'words: %s, Characters %s'" % (words, characters))
